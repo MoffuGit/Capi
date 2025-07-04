5b9db2a8bae4982218cc8319c64327c3cf2b67e5
MoffuGit <moffu.by@gmail.com>
2025-07-04 00:09:37 -0600

    added tooltip to servers

--- app/src/routes/servers/components/servers.rs
+++ app/src/routes/servers/components/servers.rs
@@ -8,6 +8,7 @@ use serde_json::json;
 use crate::components::auth::use_auth;
 use crate::components::ui::avatar::{Avatar, AvatarFallback, AvatarImage};
 use crate::components::ui::sidebar::{SidebarMenuButton, SidebarMenuItem};
+use crate::components::ui::tooltip::{ToolTip, ToolTipContent, ToolTipTrigger};
 use crate::hooks::sycn::SyncSignal;
 
 #[component]
@@ -22,36 +23,49 @@ pub fn ServersItems() -> impl IntoView {
         })
     }));
     let params = use_params_map();
-    let is_active = move |id: &String| {
-        params
-            .get()
-            .get("server")
-            .is_some_and(|server| &server == id)
-    };
     view! {
         <Show when=move || servers.signal.get().is_some()>
             <For
                 each=move || servers.signal.get().unwrap()
                 key=|server| server.id.clone()
-                let:server
-            >
-                <SidebarMenuItem>
-                    <A href=format!("/servers/{}", server.id)>
-                        <SidebarMenuButton
-                            is_active=Signal::derive(move || is_active(&server.id))
-                            size=crate::components::ui::sidebar::SidebarMenuButtonSize::Lg
-                            class="md:h-8 md:p-0"
-                        >
-                            <Avatar class="h-8 w-8 rounded-lg">
-                                <AvatarImage url=server.image_url/>
-                                <AvatarFallback class="rounded-lg select-none bg-transparent">
-                                    {server.name.chars().next()}
-                                </AvatarFallback>
-                            </Avatar>
-                        </SidebarMenuButton>
-                    </A>
-              </SidebarMenuItem>
-            </For>
+                children=move |server| {
+                    let name = StoredValue::new(server.name.clone());
+                    let id = StoredValue::new(server.id);
+                    let is_active = Signal::derive(move || {
+                        params
+                            .get()
+                            .get("server")
+                            .is_some_and(|s| s == id.get_value())
+                    });
+                    let image_url = StoredValue::new(server.image_url);
+                    view!{
+                        <SidebarMenuItem>
+                            <ToolTip>
+                                <ToolTipTrigger>
+                                    <A href=format!("/servers/{}", id.get_value())>
+                                        <SidebarMenuButton
+                                            is_active=is_active
+                                            size=crate::components::ui::sidebar::SidebarMenuButtonSize::Lg
+                                            class="md:h-8 md:p-0"
+                                        >
+                                            <Avatar class="h-8 w-8 rounded-lg">
+                                                <AvatarImage url=image_url.get_value()/>
+                                                <AvatarFallback class="rounded-lg select-none bg-transparent">
+                                                    {name.get_value().chars().next()}
+                                                </AvatarFallback>
+                                            </Avatar>
+                                        </SidebarMenuButton>
+                                    </A>
+                                </ToolTipTrigger>
+                                <ToolTipContent side_of_set=3.0 arrow=true>
+                                    {name.get_value()}
+                                </ToolTipContent>
+                            </ToolTip>
+                      </SidebarMenuItem>
+                    }
+
+                }
+            />
         </Show>
     }
 }
